5fd6fc28564353e9c72a3db225e1831d6c659752
kcsodetz <kensodetz@gmail.com>
2019-01-02 10:56:59 -0800

    Add edge case for 404 error

--- scraper.py
+++ scraper.py
@@ -2,17 +2,29 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 
-if sys.argv != 2:
-    print("[ERROR] Missing arguments")
+FAIL = '\033[91m'
+WARN = '\033[93m'
+NC = '\033[0m'
+
+if len(sys.argv) != 2:
+    print(FAIL + "[ERROR] Missing argument `username`\n")
+    print(NC + "Usage: python3 scraper.py `username`\n")
     exit(99)
 
-user = 'kcsodetz'
+user = sys.argv[1]
 url = 'https://www.devpost.com/' + user
 data = requests.get(url=url)
+
+if data.status_code != 200:
+    print(FAIL + "[ERROR] 404 Error, cannot find username {}".format(WARN + user))
+    exit(99)
+
 soup = BeautifulSoup(data.text, 'html.parser')
 
 projects = soup.find('a', {'href': '/' + user}).find('span').text.strip()
 hackathons = soup.find('a', {'href': '/' + user + '/challenges'}).find('span').text.strip()
 followers = soup.find('a', {'href': '/' + user + '/followers'}).find('span').text.strip()
 winner = soup.find_all('img', {'class': 'winner'})
-print(projects, hackathons, followers, len(winner))
+
+print("Projects:\t{}\nHackathons:\t{}\nFollowers:\t{}\nWins:\t\t{}".format(projects, hackathons, followers, len(winner)))
+
